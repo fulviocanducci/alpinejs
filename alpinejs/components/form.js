950f393a96d8c7d7fcb41dd1dd7e2f6f11871456
Fulvio Cezar Canducci Dias <fulviocanducci@hotmail.com>
2023-11-13 16:11:33 -0300

    axios

--- alpinejs/components/form.js
+++ alpinejs/components/form.js
@@ -1,5 +1,15 @@
 export default () => {
   return {
-    submit() {},
+    submit(e) {
+      e.preventDefault();
+      console.log(this.data);
+      axios.get("https://viacep.com.br/ws/01001000/json/").then((result) => console.log(result));
+    },
+    data: {
+      name: "",
+      email: "",
+      date: "",
+      value: "",
+    },
   };
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import Alpine from "alpinejs";
+import axios from "axios";
+
 import counter from "../alpinejs/components/counter.js";
+import form from "../alpinejs/components/form.js";
+import mask from "@alpinejs/mask";
+
+Alpine.plugin(mask);
 
 window.Alpine = Alpine;
+window.axios = axios;
 
 Alpine.data("counter", counter);
+Alpine.data("form", form);
 
 Alpine.start();
